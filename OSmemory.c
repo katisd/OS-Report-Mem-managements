71c83cb9ab7e2fdb579d8ca19830432e72593906
Katid Supawee <katidd17@gmail.com>
2023-08-12 16:03:28 +0700

    Add stackFunc * to show case that stack grow downward

--- OSmemory.c
+++ OSmemory.c
@@ -11,6 +11,26 @@ char globalInitialized2[20] = "globalInitialized2";
 // Global pointer variables
 char *globalMallocA = NULL;
 
+int recurStackFunc(int to, int current) {
+    printf("------------------------------\n");
+    if (to == current) {
+        return 0;
+    }
+    // Prove Stack grow downward
+    char localA[16];
+    char localB[16];
+    printf("recurStackFunc: %d\n", current);
+    printf("localA= %p\n", (void *)localA);
+    printf("localB= %p\n", (void *)localB);
+    if (localA < localB) {
+        printf("localA < localB which means stack grow downward\n");
+    } else {
+        printf("localA > localB which means stack grow upward\n");
+    }
+    recurStackFunc(to, current + 1);
+    return 0;
+}
+
 void main() {
     // to show address of global variables
     printf("========================================\nGlobal variables\n");
@@ -23,4 +43,6 @@ void main() {
     // globalUninitialized1 or globalUninitialized2
     printf("globalMallocA= %p\n", globalMallocA);
     free(globalMallocA);
+    printf("========================================\nStack variables\n");
+    recurStackFunc(3, 0);
 }
